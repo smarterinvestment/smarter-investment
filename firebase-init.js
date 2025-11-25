5c2beeb2524db91dfc7901fda2321759a27550bd
Mauricio Marin <marinbuitrago@hotmail.com>
2025-11-24 21:37:25 -0500

    Fix: Aplicar todas las correcciones de errores

--- firebase-init.js
+++ firebase-init.js
@@ -1,13 +1,13 @@
 // Firebase Configuration
 // Configuración de tu proyecto Smarter Investment
 const firebaseConfig = {
-    apiKey: "AIzaSyBxOHfgTdRfOU4i-D7aMnJ2cAk6LrT6Tbk",
-    authDomain: "smarter-investment-777f5.firebaseapp.com",
-    projectId: "smarter-investment-777f5",
-    storageBucket: "smarter-investment-777f5.appspot.com",
-    messagingSenderId: "562663698989",
-    appId: "1:562663698989:web:a64e1e5a3f8e0c8e1f3e0c",
-    measurementId: "G-YOUR_MEASUREMENT_ID"  // Opcional
+    apiKey: "AIzaSyBT3UJTHLuBQaB9kK0539-acw8ertf__vY",
+  authDomain: "smarter-investment.firebaseapp.com",
+  projectId: "smarter-investment",
+  storageBucket: "smarter-investment.firebasestorage.app",
+  messagingSenderId: "1037439323005",
+  appId: "1:1037439323005:web:43b7b89a9c4a0313c45a14",
+  measurementId: "G-DQKR8KNV2V"
 };
 
 // Initialize Firebase
